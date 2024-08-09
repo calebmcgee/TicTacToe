4fff8b4e639061467f8310ce37c0150e093b3e96
Caleb <calebmcgee06@gmail.com>
2024-08-09 11:00:33 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const Player = function(name, score){
+    this.name = name;
+    this.score = score;
+};
+
+let gameboard = {
+    round: 0,
+    board: [1,2,3,4,5,6,7,8,9]
+}
+
+const playGame = () => {
+
+};
