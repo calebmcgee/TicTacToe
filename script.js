944c5af8364ccf0ba34056f729945efc54f938e2
Caleb <calebmcgee06@gmail.com>
2024-08-16 12:06:28 -0400

    Allow player 1 to move and end game

--- script.js
+++ script.js
@@ -4,38 +4,75 @@ const Player = function(name, score, moves){
     this.moves = moves; 
 };
 
-let gameboard = {
+let gameBoard = {
     round: 0,
     board: [1,2,3,4,5,6,7,8,9]
 }
 
-
-
-const playGame = () => {
-    console.log(gameboard.board);
-    let name1 = prompt('Player 1 name');
-    let moves1 = [];
-    let user1 = new Player(name1, 0, moves1);
-    console.log(user1);
-
-    let name2 = prompt('PLayer 2 name');
-    let moves2 = [];
-    let user2 = new Player(name2, 0, moves2);
-    console.log(user2);
-
+let game = {
+    round: 0,
+    createPlayers: function(){ 
+        let name1 = prompt('Player 1 name');
+        let user1 = new Player(name1, 0, []);
     
+        let name2 = prompt('Player 2 name');
+        let user2 = new Player(name2, 0, []);
 
-};
+        console.log(user1);
+        console.log(user2);
+        return [user1, user2];
+    },
+    playRound: function(players){
+        let user1 = players[0];
+        let user2 = players[1];
+        let win = false;
+        while (win === false){
+            let move1 = prompt(`${user1.name}'s move`);
+            user1.moves.push(parseInt(move1));
+            gameBoard.board.splice(gameBoard.board.indexOf(parseInt(move1)), 1,'x');
 
-playGame();
+            console.log(user1.moves);
+            console.log(gameBoard.board);
 
+            //win = checkWin(user1.moves);
+            if (user1.moves.includes(1) && user1.moves.includes(2) && user1.moves.includes(3)){
+                return true;
+            }
+        }
+    }
 
+}
 
+game.playRound(game.createPlayers());
+
+//playRound(game.createPlayers());
+
+const checkWin = () => {
+    if (arr.moves.includes(1) && arr.moves.includes(2) && arr.moves.includes(3)){
+        return true;
+    } else if (arr.moves.includes(4) && arr.moves.includes(5) && arr.moves.includes(6)){
+        return true;
+    } else if (arr.moves.includes(7) && arr.moves.includes(8) && arr.moves.includes(9)){
+        return true;
+    } else if (arr.moves.includes(1) && arr.moves.includes(4) && arr.moves.includes(7)){
+        return true;
+    } else if (arr.moves.includes(2) && arr.moves.includes(5) && arr.moves.includes(8)){
+        return true;
+    } else if (arr.moves.includes(3) && arr.moves.includes(6) && arr.moves.includes(9)){
+        return true;
+    } else if (arr.moves.includes(1) && arr.moves.includes(5) && arr.moves.includes(9)){
+        return true;
+    } else if (arr.moves.includes(3) && arr.moves.includes(5) && arr.moves.includes(7)){
+        return true;
+    } else {
+        return false;
+    }
 /*
+
+board
 123
 456
 789
-
 
 horizontal
 123
@@ -48,7 +85,10 @@ vertical
 369
 
 diagonal
-
 159
 357
 */
+
+};
+
+
