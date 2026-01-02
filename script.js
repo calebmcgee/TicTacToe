daebedf76ad23ca7071ccb6c9ff3333463ea9289
Caleb <calebmcgee06@gmail.com>
2026-01-02 18:08:00 -0500

    Begin reformat of game code to limit global

--- script.js
+++ script.js
@@ -1,131 +1,83 @@
-const Player = function(name, score, moves){
-    this.name = name;
-    this.score = score;
-    this.moves = moves; 
-};
-
 const boardHTML = document.querySelector(".board");
 
-let gameBoard = {
-    round: 0,
-    board: [1,2,3,4,5,6,7,8,9],
-    render: function (){
-        this.board.map((elem, ind) => {
-            let tile = document.createElement("button");
-            tile.classList.add(ind + 1);
-            tile.addEventListener('click', (event) =>{
-                if(game.turn === 1){
-                    game.player1move(event.target.classList);
-                    tile.textContent = 'X';
-                } else if(game.turn === 2){
-                    game.player2move(event.target.classList);
-                    tile.textContent = 'O';
-                }      
-            });
-            boardHTML.appendChild(tile);
-            
-        });
+
+const game = (function(){
+    let turn = 0;
+    let movesDone = [];
+    let board = [1,2,3,4,5,6,7,8,9];
+
+    const changeTurn = () => {
+        if(turn == "x"){
+            turn = "o";
+        } else if (turn == "o"){
+            turn = "x";
+        } else if (turn == 0){
+            turn = "x";
+        }
     }
-}
 
-let game = {
-    round: 0,
-    turn: 0,
-    playRound: function(){
-        game.turn = 1;
-        gameBoard.render();
-        game.round += 1;
-
-    },
-    player1move: function(x){
-        let move1 = x;
-        players[0].moves.push(parseInt(move1));
-
-        gameBoard.board.splice(gameBoard.board.indexOf(parseInt(move1)), 1,'X');
-        
-        if (game.checkWin(players[0])){
-            players[0].score += 1;
-            console.log(`${players[0].name} wins round`);
-            game.turn = 0;
-        }else {
-            game.turn = 2;
+    const checkWin = () => {
+        //Check "x"
+        if (movesDone[1] == "x" && movesDone[2] == "x" && movesDone[3] == "x"){
+            return true;
+        } else if (movesDone[4] == "x" && movesDone[5] == "x" && movesDone[6] == "x"){
+            return true;
+        } else if (movesDone[7] == "x" && movesDone[8] == "x" && movesDone[9] == "x"){
+            return true;
+        } else if (movesDone[1] == "x" && movesDone[4] == "x" && movesDone[7] == "x"){
+            return true;
+        } else if (movesDone[2] == "x" && movesDone[5] == "x" && movesDone[8] == "x"){
+            return true;
+        } else if (movesDone[3] == "x" && movesDone[6] == "x" && movesDone[9] == "x"){
+            return true;
+        } else if (movesDone[1] == "x" && movesDone[5] == "x" && movesDone[9] == "x"){
+            return true;
+        } else if (movesDone[3] == "x" && movesDone[5] == "x" && movesDone[7] == "x"){
+            return true;
+        }  //Check "o"
+        else if (movesDone[1] == "o" && movesDone[2] == "o" && movesDone[3] == "o"){
+            return true;
+        } else if (movesDone[4] == "o" && movesDone[5] == "o" && movesDone[6] == "o"){
+            return true;
+        } else if (movesDone[7] == "o" && movesDone[8] == "o" && movesDone[9] == "o"){
+            return true;
+        } else if (movesDone[1] == "o" && movesDone[4] == "o" && movesDone[7] == "o"){
+            return true;
+        } else if (movesDone[2] == "o" && movesDone[5] == "o" && movesDone[8] == "o"){
+            return true;
+        } else if (movesDone[3] == "o" && movesDone[6] == "o" && movesDone[9] == "o"){
+            return true;
+        } else if (movesDone[1] == "o" && movesDone[5] == "o" && movesDone[9] == "o"){
+            return true;
+        } else if (movesDone[3] == "o" && movesDone[5] == "o" && movesDone[7] == "o"){
+            return true;
         }
-       
-    },
-    player2move: function(x){
-        let move2 = x;
-        players[1].moves.push(parseInt(move2));
-        gameBoard.board.splice(gameBoard.board.indexOf(parseInt(move2)), 1,'O');
-
-        if (game.checkWin(players[1])){
-            players[1].score += 1;
-            console.log(`${players[1].name} wins round`);
-            game.turn = 0;
-        } else {
-            game.turn = 1;
-        }
-    },
-    checkWin: function(arr){
-        if (arr.moves.includes(1) && arr.moves.includes(2) && arr.moves.includes(3)){
-            return true;
-        } else if (arr.moves.includes(4) && arr.moves.includes(5) && arr.moves.includes(6)){
-            return true;
-        } else if (arr.moves.includes(7) && arr.moves.includes(8) && arr.moves.includes(9)){
-            return true;
-        } else if (arr.moves.includes(1) && arr.moves.includes(4) && arr.moves.includes(7)){
-            return true;
-        } else if (arr.moves.includes(2) && arr.moves.includes(5) && arr.moves.includes(8)){
-            return true;
-        } else if (arr.moves.includes(3) && arr.moves.includes(6) && arr.moves.includes(9)){
-            return true;
-        } else if (arr.moves.includes(1) && arr.moves.includes(5) && arr.moves.includes(9)){
-            return true;
-        } else if (arr.moves.includes(3) && arr.moves.includes(5) && arr.moves.includes(7)){
-            return true;
-        } else {
+        //No winner
+         else {
             return false;
         }
     }
+        
+    const render = () => {
+        for (const item in board){
+            let tile = document.createElement("button");
+            tile.classList.add("board-button");
+            tile.textContent = item;
+            boardHTML.appendChild(tile);
+            
+        }
+    }
 
-}
+    const play = () => {
+        console.log("Game Start");
+        render();
+    }
 
-
-let players = [];
-function createPlayers(p1, p2){
-    let player1 = new Player (p1, 0, [])
-    let player2 = new Player (p2, 0, [])
-    return [player1, player2];
-};
-
-const p1html = document.querySelector('.p1');
-const p2html = document.querySelector('.p2');
-function playGame (form){
-    players = createPlayers(form.player1.value, form.player2.value);
-    game.playRound();
-    p1html.textContent = players[0].name;
-    p2html.textContent = players[1].name;
-};
-
-
-
-const modal = document.querySelector(".modal");
-
-const playBtn = document.querySelector(".play");
-
-playBtn.addEventListener("click", (event) => {
-    modal.showModal();
-});
-const submitButton = document.querySelector('#submit');
-submitButton.addEventListener('click', (event) => {
-  event.preventDefault();
-  modal.close();
+    return{play};
 });
 
 
-//playRound(game.createPlayers());
-
-
-
+game.play();
 
 /*
 
