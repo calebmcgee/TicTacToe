c0a95609cf5558d85fb93d1e197ac1bebf7d7fca
Caleb <calebmcgee06@gmail.com>
2026-01-05 16:14:56 -0500

    Complete Full Updated Game

--- script.js
+++ script.js
@@ -2,9 +2,14 @@
     const boardHTML = document.querySelector(".board");
     const statusBar = document.querySelector(".statusBar");
     const playBTN = document.querySelector(".play");
+    const sumbitBTN = document.querySelector("#submit");
 
     playBTN.addEventListener("click", (event)=>{
         game.startGame();
+    });
+
+    sumbitBTN.addEventListener("click", (event)=>{
+       game.setPlayers();
     });
     // link assignPLayers
 
@@ -15,14 +20,15 @@
         turn : "x",
         movesDone : [],
         gameLive : false,
-        changeTurn : function(){
+        setTurn : function(){
             if(this.turn == "x"){
                 this.turn = "o";
             } else if (this.turn == "o"){
                 this.turn = "x";
             }
+            this.render();
         }, 
-        assignPlayers : function(p1,p2){
+        setPlayers : function(p1,p2){
             this.p1 = p1;
             this.p2 = p2;
             return {p1,p2}
@@ -43,81 +49,100 @@
                     }
                 }
                 boardHTML.appendChild(cell);
-            //Render Status Bar
-            statusBar.textContent = this.checkWin();
 
+                //Render Status Bar
+                this.renderStatus();
             });
         },
+        renderStatus : function (){
+            statusBar.textContent = this.getStatus();
+        },
         endGame : function(){
-            this.turn = "x";
-            this.board = [0,1,2,3,4,5,6,7,8];
             this.gameLive = false;
         },
-        setMessage : function (message){
-            this.message = message;
-            return {message};
-        },
-        checkWin : function(){
+        getStatus : function(){
             //Check "x"
             if (this.board[0] == "x" && this.board[1] == "x" && this.board[2] == "x"){
-                return true;
+                this.endGame();
+                return "Player 1 Wins !";
             } else if (this.board[3] == "x" && this.board[4] == "x" && this.board[5] == "x"){
+                this.endGame();
                 return "Player 1 Wins !";
             } else if (this.board[6] == "x" && this.board[7] == "x" && this.board[8] == "x"){
+                this.endGame();
                 return "Player 1 Wins !";
             } else if (this.board[0] == "x" && this.board[3] == "x" && this.board[6] == "x"){
+                this.endGame();
                 return "Player 1 Wins !";
             } else if (this.board[1] == "x" && this.board[4] == "x" && this.board[7] == "x"){
+                this.endGame();
                 return "Player 1 Wins !";
             } else if (this.board[2] == "x" && this.board[5] == "x" && this.board[8] == "x"){
+                this.endGame();
                 return "Player 1 Wins !";
             } else if (this.board[0] == "x" && this.board[4] == "x" && this.board[8] == "x"){
+                this.endGame();
                 return "Player 1 Wins !";
             } else if (this.board[2] == "x" && this.board[4] == "x" && this.board[6] == "x"){
+                this.endGame();
                 return "Player 1 Wins !";
             }  //Check "o"
             else if (this.board[0] == "o" && this.board[1] == "o" && this.board[2] == "o"){
+                this.endGame();
                 return "Player 2 Wins !";
             } else if (this.board[3] == "o" && this.board[4] == "o" && this.board[5] == "o"){
+                this.endGame();
                 return "Player 2 Wins !";
             } else if (this.board[6] == "o" && this.board[7] == "o" && this.board[8] == "o"){
+                this.endGame();
                 return "Player 2 Wins !";
             } else if (this.board[0] == "o" && this.board[3] == "o" && this.board[6] == "o"){
+                this.endGame();
                 return "Player 2 Wins !";
             } else if (this.board[1] == "o" && this.board[4] == "o" && this.board[7] == "o"){
+                this.endGame();
                 return "Player 2 Wins !";
             } else if (this.board[2] == "o" && this.board[5] == "o" && this.board[8] == "o"){
+                this.endGame();
                 return "Player 2 Wins !";
             } else if (this.board[0] == "o" && this.board[4] == "o" && this.board[8] == "o"){
+                this.endGame();
                 return "Player 2 Wins !";
             } else if (this.board[2] == "o" && this.board[4] == "o" && this.board[6] == "o"){
+                this.endGame();
                 return "Player 2 Wins !";
-            } else if (!this.board.some(isNaN)){
+            } else if (this.board.every(isNaN)){
+                this.endGame();
                 return "Draw !";
             }
             //No winner
-                else {
+            else {
+                console.log(this.board);
                 return "It is " + this.turn + "'s turn.";
             }
         }, 
-        updateStatus : function(){
-            
-        },
         assignEvent : function(cell){
             cell.addEventListener("click", (e) => {
-                this.changeTurn();
-                this.changeCell(e.target.id.split("cell-")[1]);
+                this.clickCell(e.target.id.split("cell-")[1]);
             });
         },
-        changeCell : function(currentCell){
+        clickCell : function(currentCell){
             this.board[currentCell] = this.turn;
+            this.setTurn();
             this.render();
         },
         startGame : function(){
             this.endGame();
-            this.gameLive = true;
+            this.resetGame();
             this.render();
         },
+        resetGame : function(){
+            this.board = [0,1,2,3,4,5,6,7,8];
+            this.turn = "x";
+            this.movesDone = [];
+            this.gameLive = true;
+        },
+        
     }
 
 })();
